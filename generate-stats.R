d16c5db0afbebe5525a60d0b2a83f4fd3c5e2f3e
Jeremy D. Gerdes <seakintruth@gmail.com>
2021-05-29 00:09:55 -0400

    intial generate-stats.R

--- generate-stats.R
+++ generate-stats.R
@@ -1,9 +1,15 @@
 #!/usr/bin/env Rscript
 # Requires installation of R, at a minimum for apt use:
 #     $sudo apt install r-base r-base-core r-recommended
+# Api stuff needs:
+#     $sudo apt install libssl-dev openssl curl libcurl4-openssl-dev
 # Version 0.1
+# Script arguments are in this order: (default is use development keys)
+# 1) fUseProduction == "TRUE"
+# 2) secretsFilePathToSource == 'path/api-key-filepath.R'
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(
+  httr, jsonlite, digest, askpass,argparse,
   ggplot2, data.table, dplyr, tidyverse, anytime, 
   stringr, loggit, gt, webshot
 )
@@ -11,6 +17,64 @@ pacman::p_load(
 suppressMessages(
  webshot::install_phantomjs(force=FALSE)
 )
+# Get script arguments if they exist
+args <- commandArgs(trailingOnly=TRUE)
+
+#############
+# API Setup #
+#############
+# Set key and secret
+default_KeySecretFilePath <- "credentials/secrets.R"
+if(file.exists(default_KeySecretFilePath)){
+    source(default_KeySecretFilePath)
+} else {
+    if(interactive()) { 
+        api_client_key <- askpass::askpass(
+            prompt = 'ENTER YOUR PODCAST INDEX API KEY HERE: '
+        )
+        api_client_secret <- askpass::askpass(
+            prompt = 'ENTER YOUR PODCAST API SECRET HERE: '
+        )
+    } else {
+        # Handle cli Arguments if they exist
+        if(length(args)>1){
+            KeySecretFilePath <- args[2]
+            if(file.exists(KeySecretFilePath)){
+                source(KeySecretFilePath)
+            } else {
+                message("Usage: \n ./generate-stat.R 'fUseDevelopment' 'api-key-filepath.R' ")
+            }
+        } else {
+            message("Usage: \n ./generate-stat.R 'fUseDevelopment' 'api-key-filepath.R' ")
+        }
+    }
+}
+if (length(args)>0){
+    fUseProduction <- args[1] == "TRUE"
+} else {
+    fUseProduction <- FALSE
+}
+if (fUseProduction){
+    message("Running with Production keys")
+    if (!(exists("client_key"))) {
+        api_client_key <- api_production_client_key
+    }
+    if (!(exists("client_secret"))) {
+        api_client_secret <- api_production_client_secret
+    }
+} else {
+    message("Running with Development keys")
+    if (!(exists("client_key"))) {
+        api_client_key <- api_development_client_key
+    }
+    if (!(exists("client_secret"))) {
+        api_client_secret <- api_development_client_secret
+    }    
+}
+
+# For testing
+# message("Secret=",api_client_secret,"\nKey=",api_client_key)
+
 
 ###################
 # Local Functions #
@@ -112,3 +176,5 @@ suppressMessages(
     )
   )
 }
+
+
